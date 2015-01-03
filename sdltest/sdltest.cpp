7b32e5e06ac0dd1806d2e8840fd65f0f268b13d2
Vladislav Yaroslavlev <vladon@vladon.com>
2015-01-03 23:08:21 +0300

    .

--- sdltest/sdltest.cpp
+++ sdltest/sdltest.cpp
@@ -1,24 +1,90 @@
 // Use SDL and standard IO
 #include <SDL.h>
 #include <stdio.h>
+#include <curl\curl.h>
 
 // some constants
 const int WIN_WIDTH = 640;
 const int WIN_HEIGHT = 480;
 
-int main(int argc, char* args[]) {
-	
-	// window
-	SDL_Window* window = NULL;
+// functions
+bool init();
+void close();
 
-	// surface
-	SDL_Surface* surface = NULL;
+// global vars
+SDL_Window* gWindow = NULL;
+SDL_Surface* gSurface = NULL;
+SDL_Renderer* gRenderer = NULL;
 
-	// Init SDL, only video
-	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+SDL_Rect topViewport;
+
+bool init()
+{
+	bool success = true;
+
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		printf("SDL could not initialize. SDL Error: %s\n", SDL_GetError());
+		success = false;
 	}
-	else {
-
+	else
+	{
+		gWindow = SDL_CreateWindow("SDL test", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIN_WIDTH, WIN_HEIGHT, SDL_WINDOW_SHOWN);
+		if (gWindow == NULL)
+		{
+			printf("Window could not be created. SDL Error: %s\n", SDL_GetError());
+			success = false;
+		}
+		else
+		{
+			gSurface = SDL_GetWindowSurface(gWindow);
+		}
 	}
+
+	return success;
+}
+
+void close()
+{
+	SDL_DestroyWindow(gWindow);
+	gWindow = NULL;
+
+	SDL_Quit();
+}
+
+int main(int argc, char* args[]) 
+{	
+	if (!init())
+	{
+		printf("Failed to initialize.\n");
+	}
+	else
+	{
+		topViewport.x = 0;
+		topViewport.y = 0;
+		topViewport.w = WIN_WIDTH;
+		topViewport.h = WIN_HEIGHT / 2;
+
+
+		bool quit = false;
+
+		SDL_Event e;
+
+		while (!quit)
+		{
+			while (SDL_PollEvent(&e) != 0)
+			{
+				if (e.type == SDL_QUIT)
+				{
+					quit = true;
+				}
+			}
+
+			SDL_UpdateWindowSurface(gWindow);
+		}
+	}
+
+	close();
+
+	return 0;
 }
